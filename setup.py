cd6dce417d77c4e5bf007bda9019e9a831f9a9c0
Paul Kern <pkern@laas.fr>
2023-03-01 14:45:54 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fi:
+    long_description = fi.read()
+
+setuptools.setup(
+	name="chplot",
+	version="1.0.0",
+	author="Paul 'charon25' Kern",
+	description="Fast plot any math expression",
+	long_description=long_description,
+    long_description_content_type='text/markdown',
+	python_requires=">=3.9",
+	url="TODO",
+	license="MIT",
+	packages=['chplot'],
+	download_url="TODO"
+)
