4a8364505714a25faf1b738c14554ba158035dbd
Paul Kern <paul.kern@insa-strasbourg.fr>
2023-04-20 16:33:53 +0200

    Moved functions from mpmath to scipy.special because a lot faster

--- chplot/functions/mpmath_functions.py
+++ chplot/functions/mpmath_functions.py
@@ -4,51 +4,36 @@ from chplot.functions.utils import FunctionDict, get_functions_from_module, get_
 
 
 _MPMATH_MODULE_FUNCTION_NAMES: tuple[int, str] = [
-    (1, 'lambertw'),
-    (2, 'agm'),
-
     (1, 'sec'), (1, 'csc'), (1, 'cot'),
     (1, 'asec'), (1, 'acsc'), (1, 'acot'),
     (1, 'cospi'), (1, 'cospi'),
-    (1, 'sinc'), (1, 'sincpi'),
+    (1, 'sinc'),
     (1, 'sech'), (1, 'csch'), (1, 'coth'),
     (1, 'asech'), (1, 'acsch'), (1, 'acoth'),
 
-    (1, 'factorial'), (1, 'fac'), (1, 'fac2'),
-    (2, 'binomial'),
-    (1, 'rgamma'), (1, 'loggamma'), (3, 'gammainc'),
+    (1, 'fac2'),
     (1, 'rf'), (1, 'ff'),
-    (2, 'beta'), (4, 'betainc'),
     (1, 'superfac'), (1, 'hyperfac'), (1, 'barnesg'),
-    (1, 'digamma'),
     (1, 'harmonic'),
 
-    (3, 'gammainc'),
     (1, 'li'),
-    (1, 'erfi'), (1, 'erfinv'),
-    (3, 'npdf'), (3, 'ncdf'),
-    (1, 'fresnels'), (1, 'fresnelc'),
 
-    (2, 'besselj'), (2, 'bessely'), (2, 'besseli'), (2, 'besselk'),
-    (2, 'ber'), (2, 'bei'), (2, 'ker'), (2, 'kei'),
-    (2, 'struveh'), (2, 'struvel'),
     (2, 'angerj'), (2, 'webere'),
     (3, 'lommels1'), (3, 'lommels2'),
-    (2, 'airyai'), (2, 'airybi'),
     (1, 'scorergi'), (1, 'scorerhi'),
     (3, 'coulombf'), (3, 'coulombg'), (2, 'coulombc'),
-    (3, 'hyperu'), (3, 'whitm'), (3, 'whitw'),
+    (3, 'whitm'), (3, 'whitw'),
     (2, 'pcfd'), (2, 'pcfu'), (2, 'pcfv'), (2, 'pcfw'),
 
     (2, 'legendre'), (3, 'legenp'), (3, 'legenq'),
     (2, 'chebyt'), (2, 'chebyu'),
     (4, 'jacobi'), (3, 'gegenbauer'), (2, 'hermite'), (3, 'laguerre'),
 
-    (2, 'hyp0f1'), (3, 'hyp1f1'), (4, 'hyp1f2'), (3, 'hyp2f0'), (4, 'hyp2f1'), (5, 'hyp2f3'), (6, 'hyp3f2'),
+    (4, 'hyp1f2'), (3, 'hyp2f0'), (5, 'hyp2f3'), (6, 'hyp3f2'),
 
-    (2, 'ellipk'), (2, 'ellipf'), (2, 'ellipe'), (3, 'ellippi'), (3, 'elliprf'), (2, 'elliprc'), (4, 'elliprj'), (3, 'elliprd'), (3, 'elliprg'),
+    (2, 'ellipf'), (3, 'ellippi'),
 
-    (1, 'zeta'), (1, 'altzeta'), (1, 'stieltjes'), (1, 'siegelz'), (1, 'siegeltheta'),
+    (1, 'altzeta'), (1, 'stieltjes'), (1, 'siegelz'), (1, 'siegeltheta'),
     (1, 'backlunds'), (3, 'lerchphi'),
     (2, 'polylog'), (2, 'clsin'), (2, 'clcos'), (2, 'polyexp'),
     (1, 'primezeta'), (1, 'secondzeta'),
@@ -56,15 +41,16 @@ _MPMATH_MODULE_FUNCTION_NAMES: tuple[int, str] = [
     (1, 'fib'), (1, 'fibonacci'),
     (1, 'primepi'), (1, 'riemannr'),
 ]
+
 _MPMATH_MODULE_FUNCTION_RENAMED: tuple[str, int, str] = {
-    ('betainc0', 4, 'betainc'),
+    ('betainc2', 4, 'betainc'),
 
     ('W', 1, 'lambertw'),
     ('Ei', 1, 'ei'),
-    ('Ci', 1, 'ci'), ('Si', 1, 'si'),
-    ('Chi', 1, 'chi'), ('Shi', 1, 'shi'),
 
     ('normsinc', 1, 'sincpi'), ('nsinc', 1, 'sincpi'),
+
+    ('gammainc2', 3, 'gammainc'),
 
     ('hurwitz', 2, 'zeta'), ('hurwitzzeta', 2, 'zeta'), ('eta', 1, 'altzeta'), ('nzetazeros', 1, 'nzeros')
 
@@ -74,14 +60,10 @@ _MPMATH_MODULE_FUNCTION_RENAMED: tuple[str, int, str] = {
 def _Li(x: float) -> float:
     return mpmath.li(x, offset=True)
 
-def _psi(x: float) -> float:
-    return mpmath.psi(0, x)
-
 MPMATH_FUNCTIONS: FunctionDict = get_functions_from_module(mpmath, _MPMATH_MODULE_FUNCTION_NAMES)
 
 MPMATH_FUNCTIONS.update({
     'Li': (1, _Li),
-    'psi': (1, _psi),
 })
 
 MPMATH_FUNCTIONS.update(get_renamed_functions_from_module(mpmath, _MPMATH_MODULE_FUNCTION_RENAMED))
--- chplot/functions/scipy_functions.py
+++ chplot/functions/scipy_functions.py
@@ -1,13 +1,92 @@
 import scipy.special
 
-from chplot.functions.utils import FunctionDict, get_functions_from_module
+from chplot.functions.utils import FunctionDict, get_functions_from_module, get_renamed_functions_from_module
 
 
 
 _SCIPY_SPECIAL_MODULE_FUNCTION_NAMES: tuple[int, str] = [
+    (2, 'agm'),
+    (1, 'lambertw'),
+
+    (1, 'factorial'), 
+    (2, 'binom'),
+
+    (1, 'rgamma'), (1, 'gammaln'), (1, 'loggamma'),
+    (2, 'gammainc'), (2, 'gammaincinv'), (2, 'gammaincc'), (2, 'gammainccinv'),
+    (1, 'psi'), (1, 'digamma'),
+
+    (2, 'beta'), (2, 'betaln'), (3, 'betainc'), (3, 'betaincinv'),
+
+    (1, 'erfi'), (1, 'erfinv'),
     (1, 'erfcinv'),
-    (3, 'betaincinv'),
-    (1, 'psi'), (1, 'digamma')
+
+    (1, 'ber'), (1, 'bei'), (1, 'berp'), (1, 'beip'),
+    (1, 'ker'), (1, 'kei'), (1, 'kerp'), (1, 'keip'),
+    (2, 'jv'), (2, 'yv'), (2, 'kv'), (2, 'iv'),
+
+    (2, 'struve'), (2, 'modstruve'),
+    (1, 'itstruve0'), (1, 'it2struve0'), (1, 'itmodstruve0'),
+
+    (3, 'hyperu'), (2, 'hyp0f1'), (3, 'hyp1f1'), (4, 'hyp2f1'),
+
+    (1, 'ellipk'), (2, 'ellipkinc'), (1, 'ellipe'), (2, 'ellipeinc'), (3, 'elliprf'), (2, 'elliprc'), (4, 'elliprj'), (3, 'elliprg'), (3, 'elliprd'),
+
+    (1, 'zeta'),
+
 ]
 
+_SCIPY_SPECIAL_MODULE_FUNCTION_RENAMED: tuple[str, int, str] = {
+    ('binomial', 2, 'binom'),
+
+    ('normsinc', 1, 'sinc'), ('nsinc', 1, 'sinc'), ('sincpi', 1, 'sinc'),
+
+    ('fac', 1, 'factorial'),
+
+    ('besselj', 2, 'jv'), ('bessely', 2, 'yv'), ('besselk', 2, 'kv'), ('besseli', 2, 'iv'),
+
+    ('struveh', 2, 'struve'), ('struvel', 2, 'modstruve'),
+
+    ('hurwitz', 2, 'zeta'),
+
+}
+
+def _fresnels(x: float) -> float:
+    return scipy.special.fresnel(x)[0]
+def _fresnelc(x: float) -> float:
+    return scipy.special.fresnel(x)[1]
+
+def _Ai(x: float) -> float:
+    return scipy.special.airy(x)[0]
+def _Bi(x: float) -> float:
+    return scipy.special.airy(x)[1]
+def _Aip(x: float) -> float:
+    return scipy.special.airy(x)[2]
+def _Bip(x: float) -> float:
+    return scipy.special.airy(x)[3]
+
+def _Si(x: float) -> float:
+    return scipy.special.sici(x)[0]
+def _Ci(x: float) -> float:
+    return scipy.special.sici(x)[1]
+
+def _Shi(x: float) -> float:
+    return scipy.special.shichi(x)[0]
+def _Chi(x: float) -> float:
+    return scipy.special.shichi(x)[1]
+
 SCIPY_SPECIAL_FUNCTIONS: FunctionDict = get_functions_from_module(scipy.special, _SCIPY_SPECIAL_MODULE_FUNCTION_NAMES)
+
+SCIPY_SPECIAL_FUNCTIONS.update({
+    'fresnels': (1, _fresnels),
+    'fresnelc': (1, _fresnelc),
+    'Ai': (1, _Ai),
+    'Aip': (1, _Aip),
+    'Bi': (1, _Bi),
+    'Bip': (1, _Bip),
+    'Si': (1, _Si),
+    'Ci': (1, _Ci),
+    'Shi': (1, _Shi),
+    'Chi': (1, _Chi),
+})
+
+SCIPY_SPECIAL_FUNCTIONS.update(get_renamed_functions_from_module(scipy.special, _SCIPY_SPECIAL_MODULE_FUNCTION_RENAMED))
