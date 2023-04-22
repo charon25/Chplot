Added triangle distribution ; added relative error to regression computation ; corrected bug about regression

chplot/functions/probability_functions.py
@@ -21,6 +21,24 @@ def _unit_norm_cdf(x: float) -> float:
     # 1.4142135623730951 = sqrt(2)
     return 0.5 * (1 + math.erf(x / 1.4142135623730951))
 
+
+def _triangle_pdf(x: float, a: float, b: float, c: float) -> float:
+    if x < a or x > b:
+        return 0
+    if x <= c:
+        return 2 * (x - a) / ((b - a) * (c - a))
+    return 2 * (b - x) / ((b - a) * (b - c))
+
+def _triangle_cdf(x: float, a: float, b: float, c: float) -> float:
+    if x < a:
+        return 0
+    if x <= c:
+        return ((x - a) ** 2) / ((b - a) * (c - a))
+    if x <= b:
+        return 1 - ((b - x) ** 2) / ((b - a) * (b - c))
+    return 1
+
+
 def _uniform_pdf(x: float, a: float, b: float) -> float:
     return 1 / (b - a) if a <= x <= b else 0
 
@@ -29,11 +47,13 @@ def _uniform_cdf(x: float, a: float, b: float) -> float:
         return (x - a) / (b - a)
     return 0 if x < a else 1
 
-def _expon_pdf(x: float, lamb: float) -> float:
-    return lamb * math.exp(-lamb * x) if x >= 0 else 0
 
-def _expon_cdf(x: float, lamb: float) -> float:
-    return 1 - math.exp(-lamb * x) if x >= 0 else 0
+def _expon_pdf(x: float, _lambda: float) -> float:
+    return _lambda * math.exp(-_lambda * x) if x >= 0 else 0
+
+def _expon_cdf(x: float, _lambda: float) -> float:
+    return 1 - math.exp(-_lambda * x) if x >= 0 else 0
+
 
 def _cauchy_pdf(x: float, x0: float, gamma: float) -> float:
     return 1 / (math.pi * gamma * (1 + ((x - x0) / gamma) ** 2))
@@ -42,17 +62,20 @@ def _cauchy_cdf(x: float, x0: float, gamma: float) -> float:
     # 0.3183098861837907 = 1 / pi
     return 0.5 + 0.3183098861837907 * math.atan((x - x0) / gamma)
 
+
 def _student_pdf(x: float, nu: float) -> float:
     return scipy.stats.t.pdf(x, nu)
 
 def _student_cdf(x: float, nu: float) -> float:
     return scipy.stats.t.cdf(x, nu)
 
+
 def _beta_pdf(x: float, alpha: float, beta: float) -> float:
     return scipy.stats.beta.pdf(x, alpha, beta)
 
 def _beta_cdf(x: float, alpha: float, beta: float) -> float:
     return scipy.stats.beta.cdf(x, alpha, beta)
+
 
 def _chi2_pdf(x: float, k: float) -> float:
     return scipy.stats.chi2.pdf(x, k)
@@ -74,12 +97,15 @@ PROBABILITY_FUNCTIONS: FunctionDict = {
     'unormpdf': (1, _unit_norm_pdf),
     'normcdf': (3, _norm_cdf),
     'unormcdf': (1, _unit_norm_cdf),
+    # Triangle distribution,
+    'tripdf': (4, _triangle_pdf),
+    'tricdf': (4, _triangle_cdf),
     # Uniform distribution
     'uniformpdf': (3, _uniform_pdf),
     'uniformcdf': (3, _uniform_cdf),
     # Exponential distribution
-    'exponpdf': (2, _expon_pdf),
-    'exponcdf': (2, _expon_cdf),
+    'exppdf': (2, _expon_pdf),
+    'expcdf': (2, _expon_cdf),
     # Student's t distribution
     'studentpdf': (2, _student_pdf),
     'studentcdf': (2, _student_cdf),

chplot/plot/regression.py
@@ -105,17 +105,19 @@ def _remove_nan(arr1: Union[list[float], np.ndarray], arr2: Union[list[float], n
 
 
 # Ref : https://stackoverflow.com/questions/19189362/getting-the-r-squared-value-using-curve-fit
-def _compute_r_squared_and_error(ydata: np.ndarray, yfit: np.ndarray) -> tuple[float, float]:
+def _compute_r_squared_and_errors(ydata: np.ndarray, yfit: np.ndarray) -> tuple[float, float]:
     residuals = ydata - yfit
     sum_sq_res = np.sum(residuals ** 2)
     sum_sq_tot = np.sum((ydata - np.mean(ydata)) ** 2)
 
     max_error = np.max(np.abs(residuals))
+    non_zero_indices = ~np.isclose(ydata, 0)
+    max_rel_error = np.max(np.abs(residuals[non_zero_indices] / ydata[non_zero_indices]))
 
     if sum_sq_tot == 0:
-        return (1, max_error)
+        return (1, max_error, max_rel_error)
 
-    return (1 - sum_sq_res / sum_sq_tot, max_error)
+    return (1 - sum_sq_res / sum_sq_tot, max_error, max_rel_error)
 
 
 def compute_regressions(parameters: PlotParameters, graphs: list[Graph]) -> list[Graph]:
@@ -174,11 +176,16 @@ def compute_regressions(parameters: PlotParameters, graphs: list[Graph]) -> list
             LOGGER.error("error while computing regression of '%s', try reducing the number of parameters or simplifying the expression", graph.expression)
             continue
 
+        if np.isnan(parameters_values).any():
+            pbar.close()
+            LOGGER.error("error while computing regression of '%s', try changing the number of parameters or simplifying the expression", graph.expression)
+            continue
+
         pbar.close()
 
         custom_inputs = np.linspace(graph.inputs.min(), graph.inputs.max(), parameters.n_points, endpoint=True)
 
-        r2, max_error = _compute_r_squared_and_error(*_remove_nan(values_without_nan, _regression_function(inputs_without_nan, *parameters_values)))
+        r2, max_error, max_rel_error = _compute_r_squared_and_errors(*_remove_nan(values_without_nan, _regression_function(inputs_without_nan, *parameters_values)))
 
         regression_graphs.append(Graph(
             inputs=custom_inputs,
@@ -197,6 +204,7 @@ def compute_regressions(parameters: PlotParameters, graphs: list[Graph]) -> list
         file.write(f'\n  Accuracy on [{graph.inputs.min():.3f} ; {graph.inputs.max():.3f}]:\n')
         file.write(f'    R2 = {r2}\n')
         file.write(f'    |err| <= {max_error}\n')
+        file.write(f'    |rel err| <= {max_rel_error}\n')
         file.write(f'\n  Copyable expression:\n    f(x) = {_get_fit_expression(parameters.regression_expression, parameters_names, parameters_values)}\n\n\n')
 
     return regression_graphs