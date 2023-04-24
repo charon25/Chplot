8e23599b1cc254b58cd8781219f6dc7797f3f67f
Paul Kern <paul.kern@insa-strasbourg.fr>
2023-04-24 15:41:47 +0200

    Removed error computation from integrals

--- chplot/plot/integral.py
+++ chplot/plot/integral.py
@@ -1,9 +1,7 @@
-import math
 import sys
 
 import numpy as np
 
-from chplot.plot.derivative import _get_second_derivative
 from chplot.plot.plot_parameters import PlotParameters
 from chplot.plot.utils import _round as round
 from chplot.plot.utils import Graph, GraphType
@@ -11,7 +9,7 @@ from chplot.plot.utils import LOGGER
 
 
 # See https://en.wikipedia.org/wiki/Trapezoidal_rule for the computation of the integral
-def _compute_integral(parameters: PlotParameters, graph: Graph) -> tuple[float, float]:
+def _compute_integral(parameters: PlotParameters, graph: Graph) -> float:
     delta = graph.inputs[1] - graph.inputs[0]
     # use numpy as it's faster even with the conversion
     values = np.nan_to_num(graph.values, copy=True, nan=0)
@@ -19,11 +17,7 @@ def _compute_integral(parameters: PlotParameters, graph: Graph) -> tuple[float, 
     integral = sum(values[1:-1]) + (values[0] + values[-1]) / 2
     integral *= delta
 
-    second_derivative_abs_maximum = np.max(np.abs(_get_second_derivative(values, delta)))
-
-    abs_error = second_derivative_abs_maximum * ((graph.inputs[-1] - graph.inputs[0]) ** 3) / (12 * (parameters.n_points ** 2))
-
-    return (float(integral), float(abs_error))
+    return float(integral)
 
 
 def compute_and_print_integrals(parameters: PlotParameters, graphs: list[Graph]):
@@ -42,14 +36,13 @@ def compute_and_print_integrals(parameters: PlotParameters, graphs: list[Graph])
     # file.write(f'\nThe integral of the function{"s" if len(graphs) > 1 else ""}...\n\n')
     for graph in graphs:
         try:
-            integral, abs_error = _compute_integral(parameters, graph)
-            max_decimal_places = abs(math.floor(1 + math.log10(abs_error)))
+            integral = _compute_integral(parameters, graph)
         except Exception:
             LOGGER.error("error while computing integral for expression '%s'", graph.expression)
             continue
 
         file.write('\n')
-        file.write(f'- ∫f(x)dx = {round(integral, max_decimal_places)} ± {abs_error:.2e}\n    where f(x) = {graph.expression} on [{round(graph.inputs[0], 3)} ; {round(graph.inputs[-1], 3)}]\n')
+        file.write(f'- ∫f(x)dx = {integral}\n    where f(x) = {graph.expression} on [{round(graph.inputs[0], 3)} ; {round(graph.inputs[-1], 3)}]\n')
         file.write('\n')
 
     file.write('\n')
--- tests/test_integrals.py
+++ tests/test_integrals.py
@@ -22,7 +22,7 @@ class TestComputeIntegrals(unittest.TestCase):
         inputs = _generate_inputs(parameters)
         graph = _generate_graphs(parameters, inputs)[0]
 
-        self.assertTupleAlmostEqual(_compute_integral(parameters, graph), (0.5, 0.0))
+        self.assertAlmostEqual(_compute_integral(parameters, graph), 0.5)
 
     def test_exact_integral_2(self):
         parameters = MockParameters(expressions=['7*x-2'], x_lim=(-4, 3))
@@ -30,16 +30,16 @@ class TestComputeIntegrals(unittest.TestCase):
         inputs = _generate_inputs(parameters)
         graph = _generate_graphs(parameters, inputs)[0]
 
-        self.assertTupleAlmostEqual(_compute_integral(parameters, graph), (-38.5, 0.0))
+        self.assertAlmostEqual(_compute_integral(parameters, graph), -38.5)
 
     def test_integral_polynome(self):
         parameters = MockParameters(expressions=['3*x^3-2*x^2+x-10'], x_lim=(-2, 6))
         set_default_values(parameters)
         inputs = _generate_inputs(parameters)
         graph = _generate_graphs(parameters, inputs)[0]
-        integral, abs_error = _compute_integral(parameters, graph)
+        integral = _compute_integral(parameters, graph)
 
-        self.assertTrue(integral - abs_error <= 2240 / 3 <= integral + abs_error)
+        self.assertAlmostEqual(integral, 2240 / 3, places=3)
 
     def test_integral_half_circle(self):
         parameters = MockParameters(expressions=['sqrt(1-x^2)'], x_lim=(-1, 1))
@@ -47,9 +47,9 @@ class TestComputeIntegrals(unittest.TestCase):
         _load_functions(parameters)
         inputs = _generate_inputs(parameters)
         graph = _generate_graphs(parameters, inputs)[0]
-        integral, abs_error = _compute_integral(parameters, graph)
+        integral = _compute_integral(parameters, graph)
 
-        self.assertTrue(integral - abs_error <= math.pi / 2 <= integral + abs_error)
+        self.assertAlmostEqual(integral, math.pi / 2, places=3)
 
     def test_integral_inverse(self):
         parameters = MockParameters(expressions=['1/x'], x_lim=(1, 'e'))
@@ -57,9 +57,9 @@ class TestComputeIntegrals(unittest.TestCase):
         convert_parameters_expression(parameters)
         inputs = _generate_inputs(parameters)
         graph = _generate_graphs(parameters, inputs)[0]
-        integral, abs_error = _compute_integral(parameters, graph)
+        integral = _compute_integral(parameters, graph)
 
-        self.assertTrue(integral - abs_error <= 1.0 <= integral + abs_error)
+        self.assertAlmostEqual(integral, 1.0, places=3)
 
     def test_integral_discontinuity(self):
         parameters = MockParameters(expressions=['heaviside(x)'], x_lim=(-2, 2))
@@ -67,9 +67,9 @@ class TestComputeIntegrals(unittest.TestCase):
         _load_functions(parameters)
         inputs = _generate_inputs(parameters)
         graph = _generate_graphs(parameters, inputs)[0]
-        integral, abs_error = _compute_integral(parameters, graph)
+        integral = _compute_integral(parameters, graph)
 
-        self.assertTrue(integral - abs_error <= 2.0 <= integral + abs_error)
+        self.assertAlmostEqual(integral, 2.0, places=3)
 
     def test_integral_nan(self):
         parameters = MockParameters(expressions=['sqrt(abs(x) - 2)'], x_lim=(-4, 4))
@@ -77,6 +77,6 @@ class TestComputeIntegrals(unittest.TestCase):
         _load_functions(parameters)
         inputs = _generate_inputs(parameters)
         graph = _generate_graphs(parameters, inputs)[0]
-        integral, abs_error = _compute_integral(parameters, graph)
+        integral = _compute_integral(parameters, graph)
 
-        self.assertTrue(integral - abs_error <= 8 * math.sqrt(2) / 3 <= integral + abs_error)
+        self.assertAlmostEqual(integral, 8 * math.sqrt(2) / 3, places=3)
